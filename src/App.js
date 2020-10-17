7ef30f24595534f878817ef44dd0bca26f494d9f
kendzisah <52963626+kendzisah@users.noreply.github.com>
2020-10-16 20:09:54 -0400

    Creating A Rain Animation Background
    Trying to incorpate a Rain Animation background.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import './components/rain'
+import rain from './components/rain';
 
 function App() {
   return (
     <div className="App">
-      
+      <rain />
     </div>
   );
 }
